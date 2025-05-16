309565ed2180ca3210f5c8b25938be4cec86aaa8
wander <wandergp98@gmail.com>
2025-05-16 19:32:10 -0400

    Agregando archivos ai,py stats.py y utils.py con sus respectivos codigos funcionando

---
+++ stats.py
@@ -0,0 +1,27 @@
+from collections import Counter
+import matplotlib.pyplot as plt
+
+def mostrar_estadisticas(historial):
+    jugadas = historial["jugadas_usuario"]
+    ia_jugadas = historial["jugadas_ia"]
+
+    resultados = {"ganadas":0,"perdidas":0,"empatadas":0}
+
+    for j, ia in zip(jugadas,ia_jugadas):
+        if j == ia:
+            resultados["empatadas"] += 1
+        elif (j- ia) %3 ==1:
+            resultados["ganadas"]+= 1
+        else:
+            resultados["perdidas"] += 1
+    total = len(jugadas)
+    print("\nEstadisticas finales:")
+    print(f"\nPartidas jugadas {total}:")
+    print(f"\nGanadas:{resultados['ganadas']}")
+    print(f"\nPerdidas:{resultados['perdidas']}")
+    print(f"\nEmpatadas:{resultados['empatadas']}")
+    plt.bar(resultados.keys(), resultados.values())
+    plt.title("Resultados del juego")
+    plt.xlabel("Resultado")
+    plt.ylabel("cantidad")
+    plt.show()
---
+++ utils.py
@@ -0,0 +1,25 @@
+import pickle
+import os
+
+HISTORIAL_PATH = "historial.pkl"
+MODEL_PATH = "modelo_entrenado.pkl"
+
+def guardar_historial(historial):
+    with open(HISTORIAL_PATH,"wb") as f:
+        pickle.dump(historial,f)
+
+def cargar_historial():
+    if os.path.exists(HISTORIAL_PATH):
+        with open(HISTORIAL_PATH,"rb") as f:
+            return pickle.load(f)
+    return {"jugadas_usuario":[],"jugadas_ia":[]}
+
+def guardar_modelo(modelo):
+    with open(MODEL_PATH, "wb") as f:
+        pickle.dump(modelo,f)
+
+def cargar_modelo():
+    if os.path.exists(MODEL_PATH):
+        with open(MODEL_PATH,"rb")as f:
+            return pickle.load(f)
+    return None
